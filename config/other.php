fb59705238bbe5d6f3e87947776239ad223a6617
Who's Batman <whosbatman@proton.me>
2022-09-24 06:26:19 +0200

    fix:typo

--- config/other.php
+++ config/other.php
@@ -11,7 +11,7 @@
  * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
  */
 // TODO We have to improve the pages management system.
-// Currently it rally sucks :(
+// Currently it really sucks :(
 return [
 
     /*
