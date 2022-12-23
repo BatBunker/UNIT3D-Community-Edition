a1b6f13ceb0e0dc38b0f610f7405f41d0c6d7511
redeemerr <redeemerr@proton.me>
2022-12-23 19:00:13 +0100

    update: tmdb to use backdrops with title when possible

--- app/Services/Tmdb/TMDB.php
+++ app/Services/Tmdb/TMDB.php
@@ -15,10 +15,20 @@ namespace App\Services\Tmdb;
 
 class TMDB
 {
+    private const  IMAGE_PATH = 'https://image.tmdb.org/t/p/original';
+
     public function image($type, $array): ?string
     {
-        if (isset($array[$type.'_path'])) {
-            return 'https://image.tmdb.org/t/p/original'.$array[$type.'_path'];
+        $original_language = $array['original_language'] ?? 'en';
+
+        if (isset($array['images']['backdrops'])
+            && $this->get_backdrop($array['images']['backdrops'], $original_language) !== []
+            && $type === 'backdrop') {
+            return self::IMAGE_PATH . $this->get_backdrop($array['images']['backdrops'], $original_language)['file_path'];
+        }
+
+        if (isset($array[$type . '_path'])) {
+            return self::IMAGE_PATH . $array[$type . '_path'];
         }
 
         return null;
@@ -27,7 +37,7 @@ class TMDB
     public function trailer($array): ?string
     {
         if (isset($array['videos']['results'])) {
-            return 'https://www.youtube.com/embed/'.$array['videos']['results'][0]['key'];
+            return 'https://www.youtube.com/embed/' . $array['videos']['results'][0]['key'];
         }
 
         return null;
@@ -40,7 +50,7 @@ class TMDB
 
     public function ifExists($type, $array)
     {
-        if (isset($array[$type]) && ! empty($array[$type])) {
+        if (isset($array[$type]) && !empty($array[$type])) {
             return $array[$type];
         }
 
@@ -52,29 +62,43 @@ class TMDB
         return [
             'character' => $cast['character'],
             'credit_id' => $cast['credit_id'],
-            'gender'    => $cast['gender'],
-            'name'      => $cast['name'],
-            'order'     => $cast['order'],
-            'still'     => $this->image('profile', $cast),
+            'gender' => $cast['gender'],
+            'name' => $cast['name'],
+            'order' => $cast['order'],
+            'still' => $this->image('profile', $cast),
         ];
     }
 
     public function person_array($person): array
     {
         return [
-            'birthday'             => $this->ifExists('birthday', $person),
+            'birthday' => $this->ifExists('birthday', $person),
             'known_for_department' => $this->ifExists('known_for_department', $person),
-            'deathday'             => $this->ifExists('deathday', $person),
-            'name'                 => $this->ifExists('name', $person),
+            'deathday' => $this->ifExists('deathday', $person),
+            'name' => $this->ifExists('name', $person),
             //"also_known_as" => $person['also_known_as'] ?? null,
-            'gender'         => $this->ifExists('gender', $person),
-            'biography'      => $this->ifExists('biography', $person),
-            'popularity'     => $this->ifExists('popularity', $person),
+            'gender' => $this->ifExists('gender', $person),
+            'biography' => $this->ifExists('biography', $person),
+            'popularity' => $this->ifExists('popularity', $person),
             'place_of_birth' => $this->ifExists('place_of_birth', $person),
-            'still'          => $this->image('profile', $person),
-            'adult'          => $this->ifExists('adult', $person),
-            'imdb_id'        => $this->ifExists('imdb_id', $person),
-            'homepage'       => $this->ifExists('homepage', $person),
+            'still' => $this->image('profile', $person),
+            'adult' => $this->ifExists('adult', $person),
+            'imdb_id' => $this->ifExists('imdb_id', $person),
+            'homepage' => $this->ifExists('homepage', $person),
         ];
+    }
+
+    private function get_backdrop($data, $original_language): array
+    {
+        $returnData = [];
+        foreach ($data as $item) {
+            if (isset($item['iso_639_1']) && $item['iso_639_1'] == $original_language) {
+                $returnData = $item;
+            } elseif (isset($item['iso_639_1'])) {
+                $returnData = $item;
+            }
+
+        }
+        return $returnData;
     }
 }
