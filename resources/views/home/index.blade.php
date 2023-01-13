c1b6571a1093566936c7eb116996d3cbc12bc105
redeemerr <redeemerr@proton.me>
2023-01-14 00:05:42 +0100

    remove: ChatBox

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,20 +5,10 @@
         <span class="dividerv2 col-md-10 col-sm-10 col-md-offset-1"></span>
         @include('blocks.news')
         <span class="dividerv2 col-md-10 col-sm-10 col-md-offset-1 dividerv2--small-padding"></span>
-        @if(config('chat.show-chat'))
-
-            <div id="vue">
-                <script src="{{ mix('js/chat.js') }}" crossorigin="anonymous"></script>
-                @include('blocks.chat')
-                <span class="dividerv2 col-md-10 col-sm-10 col-md-offset-1"></span>
-            </div>
-        @endif
-
         @if(true === config('other.show-poll'))
             @include('blocks.poll')
             <span class="dividerv2 col-md-10 col-sm-10 col-md-offset-1"></span>
         @endif
-
         @include('blocks.top_torrents')
         @include('blocks.top_uploaders')
         @include('blocks.latest_topics')
