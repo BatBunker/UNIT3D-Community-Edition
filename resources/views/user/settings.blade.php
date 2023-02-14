cc30bfb484f9cca61a09bc8ca403a8f3c4644ae5
personal <varun.panchal@gmail.com>
2023-02-13 23:07:02 -0500

    Hide themes picklist on user settings

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -29,8 +29,8 @@
                 <div class="tab-content">
                     <br>
                     <form role="form" method="POST"
-                          action="{{ route('change_settings', ['username' => $user->username]) }}"
-                          enctype="multipart/form-data">
+                        action="{{ route('change_settings', ['username' => $user->username]) }}"
+                        enctype="multipart/form-data">
                         @csrf
                         <div class="well">
                             <h3>Language</h3>
@@ -39,8 +39,8 @@
                                 <label for="language" class="control-label">Language</label>
                                 <select class="form-control" id="language" name="language">
                                     @foreach (App\Models\Language::allowed() as $code => $name)
-                                        <option @if (auth()->user()->locale == $code) selected
-                                                @endif value="{{ $code }}">{{ $name }}</option>
+                                        <option @if (auth()->user()->locale == $code) selected @endif
+                                            value="{{ $code }}">{{ $name }}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -49,7 +49,7 @@
                         <div class="well">
                             <h3>Style</h3>
                             <hr>
-                            <div class="form-group">
+                            {{-- <div class="form-group">
                                 <label for="theme" class="control-label">Theme</label>
                                 <select class="form-control" id="theme" name="theme">
                                     <option @if ($user->style == 0) selected @endif value="0">Light Theme</option>
@@ -63,14 +63,14 @@
                                     <option @if ($user->style == 8) selected @endif value="8">Dark Yellow Theme</option>
                                     <option @if ($user->style == 9) selected @endif value="9">Cosmic Void Theme</option>
                                 </select>
-                            </div>
+                            </div> --}}
                             <div class="form-group">
                                 <label for="custom_css" class="control-label">External CSS Stylesheet (Stacks on top of
                                     above theme)</label>
                                 <label>
                                     <input type="text" name="custom_css" class="form-control"
-                                           value="@if ($user->custom_css) {{ $user->custom_css }}@endif"
-                                           placeholder="CSS URL">
+                                        value="@if ($user->custom_css) {{ $user->custom_css }} @endif"
+                                        placeholder="CSS URL">
                                 </label>
                             </div>
                             <div class="form-group">
@@ -78,8 +78,8 @@
                                     theme used)</label>
                                 <label>
                                     <input type="text" name="standalone_css" class="form-control"
-                                           value="@if ($user->standalone_css) {{ $user->standalone_css }}@endif"
-                                           placeholder="CSS URL">
+                                        value="@if ($user->standalone_css) {{ $user->standalone_css }} @endif"
+                                        placeholder="CSS URL">
                                 </label>
                             </div>
                         </div>
@@ -89,23 +89,25 @@
                             <hr>
                             <label for="hidden" class="control-label">Language Censor Chat?</label>
                             <div class="radio-inline">
-                                <label><input type="radio" name="censor" @if ($user->censor == 1) checked
-                                              @endif value="1">{{ __('common.yes') }}</label>
+                                <label><input type="radio" name="censor" @if ($user->censor == 1) checked @endif
+                                        value="1">{{ __('common.yes') }}</label>
                             </div>
                             <div class="radio-inline">
                                 <label><input type="radio" name="censor" @if ($user->censor == 0) checked @endif
-                                    value="0">{{ __('common.no') }}</label>
+                                        value="0">{{ __('common.no') }}</label>
                             </div>
                             <br>
                             <br>
                             <label for="hidden" class="control-label">Hide Chat?</label>
                             <div class="radio-inline">
-                                <label><input type="radio" name="chat_hidden" @if ($user->chat_hidden == 1) checked
-                                              @endif value="1">{{ __('common.yes') }}</label>
+                                <label><input type="radio" name="chat_hidden"
+                                        @if ($user->chat_hidden == 1) checked @endif
+                                        value="1">{{ __('common.yes') }}</label>
                             </div>
                             <div class="radio-inline">
-                                <label><input type="radio" name="chat_hidden" @if ($user->chat_hidden == 0) checked
-                                              @endif value="0">{{ __('common.no') }}</label>
+                                <label><input type="radio" name="chat_hidden"
+                                        @if ($user->chat_hidden == 0) checked @endif
+                                        value="0">{{ __('common.no') }}</label>
                             </div>
                         </div>
 
@@ -114,28 +116,31 @@
                             <hr>
                             <label for="torrent_layout" class="control-label">Default Torrent Layout?</label>
                             <select class="form-control" id="torrent_layout" name="torrent_layout">
-                                <option @if ($user->torrent_layout == 0) selected @endif value="0">Torrent List</option>
+                                <option @if ($user->torrent_layout == 0) selected @endif value="0">Torrent List
+                                </option>
                             </select>
                             <br>
                             <label for="poster" class="control-label">Show Posters On Torrent List View?</label>
                             <div class="radio-inline">
-                                <label><input type="radio" name="show_poster" @if ($user->show_poster == 1) checked
-                                              @endif value="1">{{ __('common.yes') }}</label>
+                                <label><input type="radio" name="show_poster"
+                                        @if ($user->show_poster == 1) checked @endif
+                                        value="1">{{ __('common.yes') }}</label>
                             </div>
                             <div class="radio-inline">
-                                <label><input type="radio" name="show_poster" @if ($user->show_poster == 0) checked
-                                              @endif value="0">{{ __('common.no') }}</label>
+                                <label><input type="radio" name="show_poster"
+                                        @if ($user->show_poster == 0) checked @endif
+                                        value="0">{{ __('common.no') }}</label>
                             </div>
                             <br>
                             <br>
                             <label for="ratings" class="control-label">Ratings Source?</label>
                             <div class="radio-inline">
-                                <label><input type="radio" name="ratings" @if ($user->ratings == 1) checked
-                                              @endif value="1">IMDB</label>
+                                <label><input type="radio" name="ratings"
+                                        @if ($user->ratings == 1) checked @endif value="1">IMDB</label>
                             </div>
                             <div class="radio-inline">
-                                <label><input type="radio" name="ratings" @if ($user->ratings == 0) checked
-                                              @endif value="0">TMDB</label>
+                                <label><input type="radio" name="ratings"
+                                        @if ($user->ratings == 0) checked @endif value="0">TMDB</label>
                             </div>
                         </div>
                         <div class="well some-padding">
